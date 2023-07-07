735e07f5a3e7380977dd4142d9b5be28f3590e0e
uyounggong <135089927+uyounggong@users.noreply.github.com>
2023-07-07 15:39:36 +0900

    test

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QComboBox
+import urllib.request
+import os
+import sys
 
+
+def update():
+    version_url = "https://uyounggong.github.io/20230707/version.txt"  # URL of your app's latest version number
+    response = urllib.request.urlopen(version_url)
+    latest_version = response.read().decode('utf-8').strip()
+
+    with open('version.txt', 'r') as file:
+        current_version = file.read().replace('\n', '')
+
+    print(current_version)
+    print(latest_version)
+    # if current_version != latest_version:
+    #     print("New update available! Updating...")
+    #     exe_url = "https://github.com/uyounggong/20230707/releases/download/<tag>/<filename>"  # URL to download the latest .exe from GitHub
+    #     urllib.request.urlretrieve(exe_url, 'latest.exe')
+    #
+    #     # Replace old exe with new one
+    #     os.remove('main.exe')
+    #     os.rename('latest.exe', 'main.exe')
+    #
+    #     # Update version file locally
+    #     with open('version.txt', 'w') as file:
+    #         file.write(latest_version)
+    #
+    #     # Restart the program
+    #     python = sys.executable
+    #     os.execl(python, python, *sys.argv)
+    # else:
+    #     print("No update available")
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
@@ -22,7 +54,7 @@ class MyWindow(QWidget):
 
 if __name__ == '__main__':
     app = QApplication([])
-
+    update()
     window = MyWindow()
     window.show()
 
